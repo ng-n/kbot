1d0b450d1a6f703d6db97a57709b083b3f6c7d90
ng-n <gubska2@gmail.com>
2023-06-13 11:42:19 +0000

    fix broken link

--- tf/modules/kind_cluster/terraform.tf
+++ tf/modules/kind_cluster/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     kind = {
-      source = "providers/justenwalker/kind"
-      version = "0.17.0"
+      source = "justenwalker/kind"
+      version = "latest"
     }
   }
 }
