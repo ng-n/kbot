e7ed10b9aef4ca46c87ca8f030bfec33a890bac3
ng-n <gubska2@gmail.com>
2023-06-13 10:10:56 +0000

    add fix

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,3 @@
 module "gke_cluster" {
-    source      = "github.com/ng-n/kbot/tree/main/tf/modules/gke_cluster"
-    GOOGLE_REGION = var.GOOGLE_REGION
-    GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  source = "git::https://github.com/ng-n/kbot.git//tf/modules/gke_cluster"
 }
--- tf/modules/gke_cluster/main.tf
+++ tf/modules/gke_cluster/main.tf
@@ -1,7 +1,6 @@
 resource "google_container_cluster" "this" {
     name        = var.GKE_CLUSTER_NAME
-    location    = var.GOOGlE_REGION
-
+    location    = var.GOOGLE_REGION
     remove_default_node_pool    = true
     initial_node_count          = 1
 }
