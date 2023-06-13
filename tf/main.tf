16699a3fae07c5fe39d9e02ee128f9efeb605571
ng-n <gubska2@gmail.com>
2023-06-13 11:25:19 +0000

    source kind cluster

--- tf/main.tf
+++ tf/main.tf
@@ -1,4 +1,4 @@
 module "gke_cluster" {
-  source = "git::https://github.com/ng-n/kbot.git//tf/modules/gke_cluster"
-  #source = "git::https://github.com/ng-n/kbot.git//tf/modules/kind_cluster"
+  #source = "git::https://github.com/ng-n/kbot.git//tf/modules/gke_cluster"
+  source = "git::https://github.com/ng-n/kbot.git//tf/modules/kind_cluster"
 }
