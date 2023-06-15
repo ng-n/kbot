315e8245bb27783f261c2173da971facb1bcdc97
ng-n <gubska2@gmail.com>
2023-06-15 08:35:07 +0000

    add kubeconfig value

--- tf/modules/gke_cluster/outputs.tf
+++ tf/modules/gke_cluster/outputs.tf
@@ -1,4 +1,6 @@
 output "kubeconfig" {
-  value       = "${path.module}/kubeconfig"
+  #value       = "${path.module}/kubeconfig"
+  value         = google_container_cluster.this.kubeconfig
+  sensitive     = true
   description = "The path to the kubeconfig file"
 }
