c593f61133c70e140b61ad27c068f4af490c4990
ng-n <gubska2@gmail.com>
2023-06-15 08:39:50 +0000

    add ref=tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source = "git::https://github.com/ng-n/kbot.git//tf/modules/gke_cluster"
+  source = "git::https://github.com/ng-n/kbot.git//tf/modules/gke_cluster?ref=tf"
   GOOGLE_REGION = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES = 2
