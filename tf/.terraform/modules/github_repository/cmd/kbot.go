a411464fb6965248e5e611ef5b726f4c15c049e5
ng-n <gubska2@gmail.com>
2023-06-16 07:15:54 +0000

    rm tf

---
+++ tf/.terraform/modules/github_repository/cmd/kbot.go
@@ -0,0 +1,121 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+// Get appVersion ex. v1.0.1 from CLI
+var appVersion = "Version"
+
+// kbotCmd represents the kbot command
+var kbotCmd = &cobra.Command{
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		fmt.Println("kbot %s started", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		// Bot Handler
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm kbot %s!", appVersion))
+			}
+			return err
+		})
+
+		// Check Bitcoin Price
+		kbot.Handle("/getbitcoinprice", func(m telebot.Context) error {
+			price, err := getBitcoinPrice()
+			if err != nil {
+				return err
+			}
+			err = m.Send(fmt.Sprintf("The current price of Bitcoin is $%.2f USD", price))
+			return err
+		})
+
+		kbot.Start()
+
+	},
+}
+
+func getBitcoinPrice() (float64, error) {
+	url := "https://api.coincap.io/v2/assets/bitcoin"
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return 0, err
+	}
+
+	priceStr := data["data"].(map[string]interface{})["priceUsd"].(string)
+	price, err := strconv.ParseFloat(priceStr, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return price, nil
+}
+func init() {
+	rootCmd.AddCommand(kbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ tf/.terraform/modules/github_repository/tf/main.tf
@@ -0,0 +1,32 @@
+module "gke_cluster" {
+  source = "git::https://github.com/ng-n/kbot.git//tf/modules/gke_cluster?ref=tf"
+  GOOGLE_REGION = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES = 2
+
+}
+
+#module "kind_cluster" {
+#  source = "git::https://github.com/ng-n/kbot.git//tf/modules/kind_cluster"
+#}
+
+module "github_repository" {
+  source    = "git::https://github.com/ng-n/kbot.git//tf/modules/github-repository"
+  github_owner      = var.GITHUB_OWNER
+  github_token      = var.GITHUB_TOKEN
+  repository_name   = var.FLUX_GITHUB_REPO
+  public_key_openssh    = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"    
+}
+
+module "flux_bootstrap" {
+  source = "git::https://github.com/ng-n/kbot.git//tf/modules/fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
+}
+
+module "tls_private_key" {
+  source    = "git::https://github.com/ng-n/kbot.git//tf/modules/hashicorp-tls-keys"
+  algorithm = "RSA"
+}
