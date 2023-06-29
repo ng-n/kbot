d50ca065b00c86dc1849ab9bdaeba8d83dd202af
ng-n <gubska2@gmail.com>
2023-06-29 17:15:52 +0100

    add clone, test, build...

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -4,6 +4,10 @@ pipeline {
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
         choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: "Pick ARCH")
     }
+    environment {
+        REPOSITORY = "https://github.com/ng-n/kbot"
+        BRANCH = "main"
+    }
     stages {
         stage('Example') {
             steps {
@@ -11,5 +15,45 @@ pipeline {
                 echo "Build for arch: ${params.ARCH}"
             }
         }
+
+        stage('clone'){
+            steps{
+                echo "Cloning repository..."
+                git branch: "${BRANCH}", url: "${REPOSITORY}"
+            }
+        }
+
+        stage('test'){
+            steps {
+                echo "Testing kbot code..."
+                sh "make test"
+            }
+        }
+
+        stage('build'){
+            steps{
+                echo "Setting kbot..."
+                sh "make build TARGETOS=${params.OS} TARGETARCH=${params.ARCH}"
+            }
+        }
+
+        stage('image'){
+            steps{
+                echo "Bulding docker image"
+                sh "make image"
+            }
+        }
+
+        stage('push'){
+            steps{
+                echo "Pushing docker image..."
+                script {
+                    docker.withRegistry('','dockerhub'){
+                        sh 'make push'
+                    }
+                }
+            }
+        }
+
     }
 }
